a6caac2353e220e69897e8e801b01f9a4046d2cb
kshitij <kshitij_987@protonmail.com>
2023-06-09 15:44:02 +0530

    added get and create route

---
+++ controllers/noteController.js
@@ -0,0 +1,91 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const asyncHandler = require("express-async-handler");
+
+//GET ALL NOTE
+const getAllNote = asyncHandler(async (req, res) => {
+  const notes = await prisma.notes.findMany({
+    select: {
+        title: true,
+        text: true,
+        userId: true,
+    }
+  });
+
+  if (!notes || notes.length === 0) {
+    return res.status(404).json({ message: "No Notes found" });
+  }
+
+  const notesWithUser = await Promise.all(
+    notes.map(async (note) => {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: note.userId,
+        },
+        select: {
+          username: true,
+        },
+      });
+      return {
+        ...note,
+        username: user.username,
+      };
+    })
+  );
+
+  res.json(notesWithUser);
+});
+
+//CREATE NEW NOTE
+const createNewNote = asyncHandler(async (req, res) => {
+  const { id, title, text } = req.body;
+
+  if (!id || !title || !text) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const notExistUser = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  if (!notExistUser) {
+    return res
+      .status(400)
+      .json({ message: "User with this id does not exist" });
+  }
+
+  const duplicateNote = await prisma.notes.findUnique({
+    where: {
+      title: title,
+    },
+  });
+
+  if (duplicateNote) {
+    return res
+      .status(400)
+      .json({ message: "Note with this title already exists" });
+  }
+
+  const newNote = await prisma.notes.create({
+    data: {
+      title: title,
+      text: text,
+      userId: id,
+    },
+  });
+  if (newNote) {
+    // Created
+    return res.status(201).json({ message: "New note created" });
+  } else {
+    return res.status(400).json({ message: "Invalid note data received" });
+  }
+});
+
+//UPDATE NOTE
+const updateNote = asyncHandler(async (req, res) => {});
+
+//DELETE NOTE
+const deleteNote = asyncHandler(async (req, res) => {});
+
+module.exports = { getAllNote, createNewNote, updateNote, deleteNote };
