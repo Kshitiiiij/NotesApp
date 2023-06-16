836b7fd61970cc1344178cad13d32f179c605a5a
kshitij <kshitij_987@protonmail.com>
2023-06-16 14:19:31 +0530

    added update and delete logic

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,16 +1,11 @@
 const { PrismaClient } = require("@prisma/client");
+const e = require("express");
 const prisma = new PrismaClient();
 const asyncHandler = require("express-async-handler");
 
 //GET ALL NOTE
 const getAllNote = asyncHandler(async (req, res) => {
-  const notes = await prisma.notes.findMany({
-    select: {
-        title: true,
-        text: true,
-        userId: true,
-    }
-  });
+  const notes = await prisma.notes.findMany({});
 
   // Check if notes exist
 
@@ -59,7 +54,7 @@ const createNewNote = asyncHandler(async (req, res) => {
       .status(400)
       .json({ message: "User with this id does not exist" });
   }
-// Check if note with this title already exists
+  // Check if note with this title already exists
   const duplicateNote = await prisma.notes.findUnique({
     where: {
       title: title,
@@ -88,9 +83,93 @@ const createNewNote = asyncHandler(async (req, res) => {
 });
 
 //UPDATE NOTE
-const updateNote = asyncHandler(async (req, res) => {});
+const updateNote = asyncHandler(async (req, res) => {
+  const { id, nId, title, text } = req.body;
+
+  if (!nId || !title || !text  || !id) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const note = await prisma.notes.findUnique({
+    where: {
+      id: nId,
+    },
+  });
+
+
+
+  if (!note) {
+    return res
+      .status(400)
+      .json({ message: "Note with this id does not exist" });
+  }
+
+  const duplicate = await prisma.notes.findUnique({
+    where: {
+      title: title,
+    },
+  });
+
+  if (duplicate && duplicate.id !== nId) {
+    return res
+      .status(400)
+      .json({ message: "Note with this title already exists" });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    }
+  })
+
+  if (!user) {
+    return res
+      .status(400)
+      .json({ message: "User with this id does not exist" });
+  }
+
+  const updatedNote = await prisma.notes.update({
+    where: {
+      id: nId,
+    },
+    data: {
+      title: title,
+      text: text,
+      userId: id,
+    },
+  });
+
+  if (updatedNote) {
+    // Created
+    return res.status(201).json({ message: "Note updated" });
+  } else {
+    return res.status(400).json({ message: "Invalid note data received" });
+  }
+
+});
 
 //DELETE NOTE
-const deleteNote = asyncHandler(async (req, res) => {});
+const deleteNote = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+
+  if (!id) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const note = await prisma.notes.delete({
+    where: {
+      id: id,
+    }
+});
+
+  if (note) {
+    // Deleted
+    return res.status(201).json({ message: "Note deleted" });
+  }
+  else {
+    return res.status(400).json({ message: "Invalid note data received" });
+  }
+
+})
 
 module.exports = { getAllNote, createNewNote, updateNote, deleteNote };
